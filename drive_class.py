f86b2b94b73b5ea8c350e57f625fbcec43847ab2
zito <52185595+zzziito@users.noreply.github.com>
2020-11-28 00:10:42 +0900

    Update drive_class.py

--- drive_class.py
+++ drive_class.py
@@ -1,200 +1,107 @@
-# -*- coding: utf-8 -*-
-from RPi import GPIO
- 
-class Drive(object):
-   def __init__(self, pins):
- 
-     
-       GPIO.setwarnings(False)
-       GPIO.setmode(GPIO.BCM)
- 
-       self.pins = pins
- 
-       GPIO.setup(self.pins['ENA_1'], GPIO.OUT)
-       GPIO.setup(self.pins['IN1_1'], GPIO.OUT)
-       GPIO.setup(self.pins['IN2_1'], GPIO.OUT)
-       GPIO.setup(self.pins['ENB_1'], GPIO.OUT)
-       GPIO.setup(self.pins['IN3_1'], GPIO.OUT)
-       GPIO.setup(self.pins['IN4_1'], GPIO.OUT)
-       GPIO.setup(self.pins['ENA_2'], GPIO.OUT)
-       GPIO.setup(self.pins['IN1_2'], GPIO.OUT)
-       GPIO.setup(self.pins['IN2_2'], GPIO.OUT)
-       GPIO.setup(self.pins['ENB_2'], GPIO.OUT)
-       GPIO.setup(self.pins['IN3_2'], GPIO.OUT)
-       GPIO.setup(self.pins['IN4_2'], GPIO.OUT)
- 
-       self.pwm1 = GPIO.PWM(self.pins['ENA_1'], 50)
-       self.pwm2 = GPIO.PWM(self.pins['ENB_1'], 50)
-       self.pwm3 = GPIO.PWM(self.pins['ENA_2'], 50)
-       self.pwm4 = GPIO.PWM(self.pins['ENB_2'], 50)
- 
-       self.pwm1.start(50)
-       self.pwm2.start(50)
-       self.pwm3.start(50)
-       self.pwm4.start(50)
-  
-   def forward(self, speed):
-       self.pwm1.ChangeDutyCycle(speed)
-       GPIO.output(self.pins['IN1_1'],True)
-       GPIO.output(self.pins['IN2_1'],False)
- 
-       self.pwm2.ChangeDutyCycle(speed)
-       GPIO.output(self.pins['IN3_1'],True)
-       GPIO.output(self.pins['IN4_1'],False)
- 
-       self.pwm3.ChangeDutyCycle(speed)
-       GPIO.output(self.pins['IN1_2'],True)
-       GPIO.output(self.pins['IN2_2'],False)
- 
-       self.pwm4.ChangeDutyCycle(speed)
-       GPIO.output(self.pins['IN3_2'],True)
-       GPIO.output(self.pins['IN4_2'],False)
- 
-   def backward(self, speed):
-       self.pwm1.ChangeDutyCycle(speed)
-       GPIO.output(self.pins['IN1_1'],False)
-       GPIO.output(self.pins['IN2_1'],True)
- 
-       self.pwm2.ChangeDutyCycle(speed)
-       GPIO.output(self.pins['IN3_1'],False)
-       GPIO.output(self.pins['IN4_1'],True)
- 
-       self.pwm3.ChangeDutyCycle(speed)
-       GPIO.output(self.pins['IN1_2'],False)
-       GPIO.output(self.pins['IN2_2'],True)
- 
-       self.pwm4.ChangeDutyCycle(speed)
-       GPIO.output(self.pins['IN3_2'],False)
-       GPIO.output(self.pins['IN4_2'],True)
-      
-   def left(self):
-       self.pwm1.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_1'],False)
-       GPIO.output(self.pins['IN2_1'],True)
-       self.pwm2.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_1'],True)
-       GPIO.output(self.pins['IN4_1'],False)
-       self.pwm3.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_2'],True)
-       GPIO.output(self.pins['IN2_2'],False)
-       self.pwm4.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_2'],False)
-       GPIO.output(self.pins['IN4_2'],True)
- 
-   def right(self):
-       self.pwm1.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_1'],True)
-       GPIO.output(self.pins['IN2_1'],False)
-       self.pwm2.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_1'],False)
-       GPIO.output(self.pins['IN4_1'],True)
-       self.pwm3.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_2'],False)
-       GPIO.output(self.pins['IN2_2'],True)
-       self.pwm4.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_2'],True)
-       GPIO.output(self.pins['IN4_2'],False)
- 
-   def leftup_diagonal(self):
-       """
-       pwm1.ChangeDutyCycle(speed)
-       GPIO.output(IN1_1,False)
-       GPIO.output(IN2_1,False)
-       """
-       self.pwm2.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_1'],True)
-       GPIO.output(self.pins['IN4_1'],False)
-       self.pwm3.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_2'],True)
-       GPIO.output(self.pins['IN2_2'],False)
-       """
-       pwm4.ChangeDutyCycle(speed)
-       GPIO.output(IN3_2,False)
-       GPIO.output(IN4_2,False)
-       """
- 
-   def rightup_diagonal(self):
-       self.pwm1.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_1'],True)
-       GPIO.output(self.pins['IN2_1'],False)
-       """
-       pwm2.ChangeDutyCycle(speed)
-       GPIO.output(IN3_1,False)
-       GPIO.output(IN4_1,False)
- 
-       pwm3.ChangeDutyCycle(speed)
-       GPIO.output(IN1_2,False)
-       GPIO.output(IN2_2,False)
- 
-       """
-       self.pwm4.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_2'],True)
-       GPIO.output(self.pins['IN4_2'],False)
- 
-   def leftdown_diagonal(self):
-       self.pwm1.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_1'],False)
-       GPIO.output(self.pins['IN2_1'],True)
-       """
-       pwm2.ChangeDutyCycle(speed)
-       GPIO.output(IN3_1,False)
-       GPIO.output(IN4_1,False)
-       pwm3.ChangeDutyCycle(speed)
-       GPIO.output(IN1_2,False)
-       GPIO.output(IN2_2,False)
-       """
-       self.pwm4.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_2'],False)
-       GPIO.output(self.pins['IN4_2'],True) 
- 
-   def rightdown_diagonal(self):
-       """
-       pwm1.ChangeDutyCycle(speed)
-       GPIO.output(IN1_1,False)
-       GPIO.output(IN2_1,False)
-       """
-       self.pwm2.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_1'],False)
-       GPIO.output(self.pins['IN4_1'],True)
-       self.pwm3.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_2'],False)
-       GPIO.output(self.pins['IN2_2'],True)
-       """
-       pwm4.ChangeDutyCycle(speed)
-       GPIO.output(IN3_2,False)
-       GPIO.output(IN4_2,False)
-       """
-   def rightturn(self):
-       self.pwm1.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_1'],True)
-       GPIO.output(self.pins['IN2_1'],False)
-       self.pwm2.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_1'],False)
-       GPIO.output(self.pins['IN4_1'],True)
-       self.pwm3.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_2'],True)
-       GPIO.output(self.pins['IN2_2'],False)
-       self.pwm4.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_2'],False)
-       GPIO.output(self.pins['IN4_2'],True)
- 
-   def leftturn(self):
-       self.pwm1.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_1'],False)
-       GPIO.output(self.pins['IN2_1'],True)
-       self.pwm2.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_1'],True)
-       GPIO.output(self.pins['IN4_1'],False)
-       self.pwm3.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN1_2'],False)
-       GPIO.output(self.pins['IN2_2'],True)
-       self.pwm4.ChangeDutyCycle(self.speed)
-       GPIO.output(self.pins['IN3_2'],True)
-       GPIO.output(self.pins['IN4_2'],False)
- 
- 
-# if __name__ == '__main__':
-#     pins = {
-#         'ENA_1': 1
-#     }
-#     car = Drive(pins=pins)
+import time
+import busio
+
+from board import SCL, SDA
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import motor
+
+
+class drivingclass:
+
+    def __init__(self):
+        print("init.............")
+        self.i2c = busio.I2C(SCL, SDA)
+
+        self.pca = PCA9685(self.i2c, address=0x40)
+        self.pca.frequency = 100
+
+        self.pca.channels[4].duty_cycle = 0xFFFF
+        self.pca.channels[9].duty_cycle = 0xFFFF
+        self.pca.channels[15].duty_cycle = 0xFFFF
+        self.pca.channels[10].duty_cycle = 0xFFFF
+
+        self.motor1 = motor.DCMotor(self.pca.channels[5], self.pca.channels[6])
+        self.motor2 = motor.DCMotor(self.pca.channels[8], self.pca.channels[7])
+        self.motor3 = motor.DCMotor(
+            self.pca.channels[14], self.pca.channels[13])
+        self.motor4 = motor.DCMotor(
+            self.pca.channels[11], self.pca.channels[12])
+        print("init.............finish")
+
+    def goForward(self, speed):
+        print(self)
+        self.motor1.throttle = speed
+        self.motor2.throttle = speed
+        self.motor3.throttle = speed
+        self.motor4.throttle = speed
+        time.sleep(1)
+
+    def goBackward(self, speed):
+        self.motor1.throttle = -speed
+        self.motor2.throttle = -speed
+        self.motor3.throttle = -speed
+        self.motor4.throttle = -speed
+        time.sleep(1)
+
+    def goRight(self, speed):
+        self.motor1.throttle = -speed
+        self.motor2.throttle = speed
+        self.motor3.throttle = -speed
+        self.motor4.throttle = speed
+        time.sleep(1)
+
+    def goLeft(self, speed):
+        self.motor1.throttle = speed
+        self.motor2.throttle = -speed
+        self.motor3.throttle = -speed
+        self.motor4.throttle = speed
+        time.sleep(1)
+
+    def goright_diagonal(self, speed):
+        self.motor1.throttle = 0
+        self.motor2.throttle = speed
+        self.motor3.throttle = 0
+        self.motor4.throttle = speed
+        time.sleep(1)
+
+    def goleft_diagonal(self, speed):
+        self.motor1.throttle = speed
+        self.motor2.throttle = 0
+        self.motor3.throttle = speed
+        self.motor4.throttle = 0
+        time.sleep(1)
+
+    def backright_diagonal(self, speed):
+        self.motor1.throttle = -speed
+        self.motor2.throttle = 0
+        self.motor3.throttle = -speed
+        self.motor4.throttle = 0
+        time.sleep(1)
+
+    def backleft_diagonal(self, speed):
+        self.motor1.throttle = 0
+        self.motor2.throttle = -speed
+        self.motor3.throttle = 0
+        self.motor4.throttle = -speed
+        time.sleep(1)
+
+    def turnRight(self, speed):
+        self.motor1.throttle = speed
+        self.motor2.throttle = -speed
+        self.motor3.throttle = -speed
+        self.motor4.throttle = speed
+        time.sleep(1)
+
+    def turnLeft(self, speed):
+        self.motor1.throttle = -speed
+        self.motor2.throttle = speed
+        self.motor3.throttle = speed
+        self.motor4.throttle = -speed
+        time.sleep(1)
+
+    def stop(self):
+        self.motor1.throttle = 0
+        self.motor2.throttle = 0
+        self.motor3.throttle = 0
+        self.motor4.throttle = 0
+        time.sleep(1)
